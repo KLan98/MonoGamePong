dd14460e402742f21e1deb12483afadb98deb8a8
ProfessionalBeginner113 <lphamkim17@gmail.com>
2026-08-02 16:02:18 +0700

    Basic implementation of rect collision, will need to refine this

--- Pong/Source/PhysicsManager.cs
+++ Pong/Source/PhysicsManager.cs
@@ -7,15 +7,15 @@ using System.Diagnostics;
 
 public class PhysicsManager
 {
-    private float maxVelocity = 180f;
+    private float maxSpeed = 300f;
     private EntityPhysics[] movingPhysics;
     private Vector2[] staticPositions;
     private static PhysicsManager instance;
     private Vector2[] sizeVector;
     private Core core;
-
+    private Rectangle[] rectColliders;
     private float[] easingTimeElapsed = new float[3]; // easing time elapsed for every moving entities 
-    private float easeDuration = 3; // time it takes to reach max speed
+    private float easeDuration = 2; // time it takes to reach max speed
 
     public PhysicsManager()
     {
@@ -25,15 +25,15 @@ public class PhysicsManager
         core = Core.GetInstance();
         Vector2 screenRes = core.GetScreenResolution();
 
-        // init physical fields of all entities
+        // init physics objects 
         Vector2 playerInitPos = new Vector2 { X = 0, Y = screenRes.Y / 2 - sizeVector[3].Y / 2 };
-        EntityPhysics playerPhysics = new EntityPhysics(playerInitPos, Vector2.Zero, 0f);
+        EntityPhysics playerPhysics = new EntityPhysics(playerInitPos, Vector2.Zero);
 
         Vector2 comIntPos = new Vector2 { X = screenRes.X - sizeVector[4].X, Y = playerInitPos.Y };
-        EntityPhysics comPhysics = new EntityPhysics(comIntPos, Vector2.Zero, 0f);
+        EntityPhysics comPhysics = new EntityPhysics(comIntPos, Vector2.Zero);
 
         Vector2 ballInitPos = new Vector2 { X = screenRes.X / 2, Y = screenRes.Y / 2 };
-        EntityPhysics ballPhysics = new EntityPhysics(ballInitPos, Vector2.Zero, 0f);
+        EntityPhysics ballPhysics = new EntityPhysics(ballInitPos, Vector2.Zero);
 
         // Init positions of non-moving objects
         Vector2 boardInitPos = Vector2.Zero;
@@ -61,6 +61,8 @@ public class PhysicsManager
             return;
         }
         instance = this;
+
+        ballPhysics.Direction = new Vector2(-1, 0);
     }
 
     public static PhysicsManager GetInstance()
@@ -93,7 +95,7 @@ public class PhysicsManager
     {
         for (int i = 0; i < movingPhysics.Length; i++)
         {
-            float currentVelocity = movingPhysics[i].Velocity;
+            float speed = movingPhysics[i].Speed;
             Vector2 direction = movingPhysics[i].Direction;
 
             float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
@@ -107,25 +109,47 @@ public class PhysicsManager
             // Easing function use normalized time as input 
             float easedTime = core.EaseInQuad(normalizedElapsed);
 
-            // lerp between current velocity and max velocity, eased time as input, clamped between current velocity and max velocity
-            float easedVelocity = MathHelper.Lerp(currentVelocity, maxVelocity, easedTime);
+            // lerp between current speed and max speed, eased time as input, clamped between current speed and max speed
+            float easedSpeed = MathHelper.Lerp(speed, maxSpeed, easedTime);
 
-            float displacement = easedVelocity * deltaTime;
+            float displacement = easedSpeed * deltaTime;
 
             Vector2 movedDistance = displacement * direction;
 
-            // update position and velocity
+            // update position and speed
             movingPhysics[i].Position += movedDistance;
-            movingPhysics[i].Velocity = easedVelocity;
+            movingPhysics[i].Speed = easedSpeed;
         }
 
         // create a new circle collision every physics update
-        Circle ballCollider = new Circle(sizeVector[5].X, movingPhysics[2].Position.X, movingPhysics[2].Position.Y);
+        //Circle ballCollider = new Circle(sizeVector[5].X, movingPhysics[2].Position.X, movingPhysics[2].Position.Y);
+        Rectangle ballCollider = new Rectangle((int)movingPhysics[2].Position.X, (int)movingPhysics[2].Position.Y, (int)sizeVector[5].X, (int)sizeVector[5].Y);
+        //Debug.WriteLine($"ball position {movingPhysics[2].Position}"); 
+        Debug.WriteLine($"{ballCollider} created at {ballCollider.Location}");
+
+        Rectangle rectCollider = new Rectangle((int)movingPhysics[0].Position.X, (int)movingPhysics[0].Position.Y, (int)sizeVector[3].X, (int)sizeVector[3].Y);
+        Debug.WriteLine($"----{rectCollider} created at {rectCollider.Location}");
+
+        if (ballCollider.Intersects(rectCollider))
+        {
+            Debug.WriteLine($"-------------------------{rectCollider}, info at intersect {rectCollider.Left}, {rectCollider.Right}, {rectCollider.Top}, {rectCollider.Bottom} collides with {ballCollider}, info at intersect {ballCollider.Left}, {ballCollider.Right}, {ballCollider.Top}, {ballCollider.Bottom}");
+        }
+
+        if (ballCollider.Location.X <= 0)
+        {
+
+            Debug.WriteLine($"-------------------------{rectCollider}, info {rectCollider.Left}, {rectCollider.Right}, {rectCollider.Top}, {rectCollider.Bottom} does not collide with {ballCollider}, info {ballCollider.Left}, {ballCollider.Right}, {ballCollider.Top}, {ballCollider.Bottom}");
+        }
+
+        //for (int i = 0; i < 2; i++)
+        //{
+        //    Rectangle rectCollider = new Rectangle((int)movingPhysics[i].Position.X, (int)movingPhysics[i].Position.Y, (int)sizeVector[1].X, (int)sizeVector[1].Y);
+        //}
     }
 
     public void MovingStop(int index)
     {
-        movingPhysics[index].Velocity = 0;
+        movingPhysics[index].Speed = 0;
         movingPhysics[index].Direction = Vector2.Zero;
         easingTimeElapsed[index] = 0;
     }
