e903b284cb7743385fb0eb92068ee01713845e2b
ProfessionalBeginner113 <lphamkim17@gmail.com>
2026-08-26 22:02:40 +0700

    Added game constants

---
+++ Pong/Source/GameConstants.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Pong
+{
+    public static class GameConstants
+    {
+        public const int DEFAULT_NUMBER_OF_BALLS = 1;
+        public const int BALL_NUMBER_SLIDER_MIN = 1;
+        public const int BALL_NUMBER_SLIDER_MAX = 500;
+    }
+}
